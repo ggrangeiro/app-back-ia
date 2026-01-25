2e6a98727309de814919129e237f70d1e325c642
Gu/Ka <guka@MacBook-Air-de-GuKa.local>
2026-01-25 10:17:31 -0300

    fix: resolve 500 error in notification route by adding @JdbcRepository annotation and fixing application.yml syntax

--- src/main/java/gcfv2/NotificationRepository.java
+++ src/main/java/gcfv2/NotificationRepository.java
@@ -1,10 +1,11 @@
 package gcfv2;
 
-import io.micronaut.data.annotation.Repository;
+import io.micronaut.data.jdbc.annotation.JdbcRepository;
+import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
 import java.util.List;
 
-@Repository
+@JdbcRepository(dialect = Dialect.MYSQL)
 public interface NotificationRepository extends CrudRepository<Notification, Long> {
 
     // Buscar notificações de um destinatário, ordenadas por mais recente
